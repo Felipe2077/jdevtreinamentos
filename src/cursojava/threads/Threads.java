818ae6e52c9b96cd10bc45f8766a2774a31d38ff
Felipe Batista <felipebatista47@hotmail.com>
2023-05-18 16:38:53 -0300

    AULA - Tempo de interrupção sleep

---
+++ src/cursojava/threads/Threads.java
@@ -0,0 +1,14 @@
+package cursojava.threads;
+
+public class Threads {
+
+    public static void main(String[] args) {
+
+      Thread.sleep(1000);
+        for (int i = 0; i < 10 ; i++) {
+
+            Thread.
+                    System.out.println("Enviando o email...");
+        }
+    }
+}
