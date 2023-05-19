f11329b5818d2ca54e2618ee875b6409458210d7
Felipe Batista <felipebatista47@hotmail.com>
2023-05-19 11:19:47 -0300

    14.5 - Thread em objeto e o Runnable

--- src/cursojava/threads/Threads.java
+++ src/cursojava/threads/Threads.java
@@ -7,45 +7,42 @@ public class Threads {
     public static void main(String[] args) throws InterruptedException {
 
         //*processo paralelo para o envio de email
-        new Thread() {
-            public void run() {
+        Thread threadEmail = new Thread(thread1);
+        threadEmail.start();
 
-                for (int i = 0; i < 10; i++) {
-                    System.out.println("Enviando o email...");
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-
-            }
-        }.start(); // liga a thread e fica processado em paralelo
-
-
-        //* Divisão das threads*****************************************************************************
-
-        //* processo paralelo para o envio NOTA FISCAL
-        new Thread() {
-            public void run() {
-
-                for (int i = 0; i < 10; i++) {
-                    System.out.println("Enviando NOTA FISCAL...");
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-
-            }
-        }.start(); // liga a thread e fica processado em paralelo
-
-
-
+        Thread threadNF = new Thread(thread2);
+        threadNF.start();
 
 
         //fluxo do sistema
         JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
     }
+
+    private static Runnable thread1 = new Runnable() {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Enviando o email...");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    };
+
+    public static Runnable thread2 = new Runnable() {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Enviando NOTA FISCAL...");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    };
 }
