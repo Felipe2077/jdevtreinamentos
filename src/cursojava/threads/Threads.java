53b62441222887918e608b8daf4bed8d453d9499
Felipe Batista <felipebatista47@hotmail.com>
2023-05-19 10:03:05 -0300

    Criando a primeira Thread

--- src/cursojava/threads/Threads.java
+++ src/cursojava/threads/Threads.java
@@ -1,13 +1,30 @@
 package cursojava.threads;
 
+import javax.swing.*;
+
 public class Threads {
 
     public static void main(String[] args) throws InterruptedException {
 
-        for (int i = 0; i < 10; i++) {
-            Thread.sleep(1000);
+        new Thread() {
+            public void run() {
 
-            System.out.println("Enviando o email...");
-        }
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Enviando o email...");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+            }
+        }.start(); // liga a thread e fica processado em paralelo
+
+
+
+
+        //fluxo do sistema
+        JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
     }
 }
