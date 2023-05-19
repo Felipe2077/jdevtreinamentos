e9f7cc8ac953884b8430b2848e56a4dace7b9c23
Felipe Batista <felipebatista47@hotmail.com>
2023-05-19 10:24:07 -0300

    14.4 - Processamento concorrente entre duas Thread

--- src/cursojava/threads/Threads.java
+++ src/cursojava/threads/Threads.java
@@ -6,11 +6,31 @@ public class Threads {
 
     public static void main(String[] args) throws InterruptedException {
 
+        //*processo paralelo para o envio de email
         new Thread() {
             public void run() {
 
                 for (int i = 0; i < 10; i++) {
                     System.out.println("Enviando o email...");
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+            }
+        }.start(); // liga a thread e fica processado em paralelo
+
+
+        //* Divisão das threads*****************************************************************************
+
+        //* processo paralelo para o envio NOTA FISCAL
+        new Thread() {
+            public void run() {
+
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Enviando NOTA FISCAL...");
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
@@ -20,6 +40,7 @@ public class Threads {
 
             }
         }.start(); // liga a thread e fica processado em paralelo
+
 
 
 
