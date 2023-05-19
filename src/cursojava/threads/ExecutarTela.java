89ffc8e05fb5dd9023caa4dbad26774544b52843
Felipe Batista <felipebatista47@hotmail.com>
2023-05-19 15:32:38 -0300

    começo da aula de tela

---
+++ src/cursojava/threads/ExecutarTela.java
@@ -0,0 +1,7 @@
+package cursojava.threads;
+
+public class ExecutarTela {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/cursojava/threads/TelaTimeThread.java
@@ -0,0 +1,13 @@
+package cursojava.threads;
+
+import javax.swing.*;
+
+public class TelaTimeThread extends JDialog {
+
+    public TelaTimeThread(){
+
+
+        setVisible(true);
+    }
+
+}
