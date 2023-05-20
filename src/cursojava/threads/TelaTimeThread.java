a73b01b9d373e8d6cfbbf5d9be77d0958cb67de4
Felipe Batista <felipebatista47@hotmail.com>
2023-05-20 10:48:25 -0300

    metade da aula de tela

--- src/cursojava/threads/TelaTimeThread.java
+++ src/cursojava/threads/TelaTimeThread.java
@@ -5,13 +5,30 @@ import java.awt.*;
 
 public class TelaTimeThread extends JDialog {
 
-    public TelaTimeThread(){
+    private JPanel jPanel = new JPanel(new GridBagLayout());
+    private JTextField mostraTempo = new JTextField();
+
+    private  JLabel descricaoHora = new JLabel("Time da Thread 1");
+
+    public TelaTimeThread() {
 
         setTitle("Minha tela de time com thread");
-        setSize(new Dimension(600,600));
+        setSize(new Dimension(600, 600));
         setLocationRelativeTo(null);
         setResizable(false);
 
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+
+        descricaoHora.setPreferredSize(new Dimension(200, 25));
+        jPanel.add(descricaoHora, gridBagConstraints);
+
+        mostraTempo.setPreferredSize(new Dimension(200, 25));
+        gridBagConstraints.gridy++;
+        jPanel.add(mostraTempo, gridBagConstraints);
+
+        add(jPanel, BorderLayout.WEST);
         setVisible(true);
     }
 
