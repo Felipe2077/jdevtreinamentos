358d1b3d9ee79936eea9cb3e55f2799ff39e8729
Felipe Batista <felipebatista47@hotmail.com>
2023-05-22 11:36:14 -0300

    15.2 - Formatando datas com o SimpleDateFormat

--- src/cursojava/ApiDateTime/DatasEmJava.java
+++ src/cursojava/ApiDateTime/DatasEmJava.java
@@ -1,10 +1,13 @@
 package cursojava.ApiDateTime;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.logging.SimpleFormatter;
 
 public class DatasEmJava {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         Date date = new Date();
 
         System.out.println("Data em milisegundos: "+date.getTime());
@@ -18,5 +21,17 @@ public class DatasEmJava {
         System.out.println("segundos: "+date.getSeconds());
 
 
+
+        /*-------------------------------simple date format-------------------------------*/
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy HH-mm.ss");
+
+        System.out.println("Data atual em formato padrão de String: "+simpleDateFormat.format(date));
+
+        simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm.ss");
+
+        System.out.println("Data para o padrão de banco de dados: "+ simpleDateFormat.format(date));
+
+        System.out.println("Objeto date: "+simpleDateFormat.parse("1987-05-18 11:34.15"));
     }
 }
