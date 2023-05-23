8d7c18033e239165c8d2deb0a2dc7fe282d54b01
Felipe Batista <felipebatista47@hotmail.com>
2023-05-23 10:20:22 -0300

    15.4 - After e Before comparando datas menores e maiores

--- src/cursojava/ApiDateTime/DatasEmJava.java
+++ src/cursojava/ApiDateTime/DatasEmJava.java
@@ -11,26 +11,26 @@ public class DatasEmJava {
         Date date = new Date();
 
         Calendar calendar = Calendar.getInstance();
-
-        System.out.println("Data em milisegundos: "+date.getTime());
-        System.out.println("Calendar em milisegundos: "+calendar.getTimeInMillis());
-
-        System.out.println("Dia do mês: "+date.getDate());
-        System.out.println("Calendar do mês: "+calendar.get(Calendar.DAY_OF_MONTH));
-
-        System.out.println("Dia da semana: "+date.getDay());
-        System.out.println("Dia da semana: "+(calendar.get(Calendar.DAY_OF_WEEK)-1));
-
-        System.out.println("hora do dia: "+date.getHours());
-        System.out.println("hora do dia: "+calendar.get(Calendar.HOUR_OF_DAY));
-
-
-        System.out.println("minuto da hora: "+date.getMinutes());
-        System.out.println("minuto da hora: "+calendar.get(Calendar.MINUTE));
-
-
-        System.out.println("segundos: "+date.getSeconds());
-        System.out.println("segundos: "+calendar.get(Calendar.SECOND));
+//
+//        System.out.println("Data em milisegundos: "+date.getTime());
+//        System.out.println("Calendar em milisegundos: "+calendar.getTimeInMillis());
+//
+//        System.out.println("Dia do mês: "+date.getDate());
+//        System.out.println("Calendar do mês: "+calendar.get(Calendar.DAY_OF_MONTH));
+//
+//        System.out.println("Dia da semana: "+date.getDay());
+//        System.out.println("Dia da semana: "+(calendar.get(Calendar.DAY_OF_WEEK)-1));
+//
+//        System.out.println("hora do dia: "+date.getHours());
+//        System.out.println("hora do dia: "+calendar.get(Calendar.HOUR_OF_DAY));
+//
+//
+//        System.out.println("minuto da hora: "+date.getMinutes());
+//        System.out.println("minuto da hora: "+calendar.get(Calendar.MINUTE));
+//
+//
+//        System.out.println("segundos: "+date.getSeconds());
+//        System.out.println("segundos: "+calendar.get(Calendar.SECOND));
 
 
 
@@ -38,13 +38,29 @@ public class DatasEmJava {
 
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy HH-mm.ss");
 
+//
+//        System.out.println("Data atual em formato padrão de String: "+simpleDateFormat.format(date));
+//        System.out.println("Calendar atual: "+calendar.getInstance().getTime());
+//        simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm.ss");
+//
+//        System.out.println("Data para o padrão de banco de dados: "+ simpleDateFormat.format(date));
+//
+//        System.out.println("Objeto date: "+simpleDateFormat.parse("1987-05-18 11:34.15"));
 
-        System.out.println("Data atual em formato padrão de String: "+simpleDateFormat.format(date));
-        System.out.println("Calendar atual: "+calendar.getInstance().getTime());
-        simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm.ss");
 
-        System.out.println("Data para o padrão de banco de dados: "+ simpleDateFormat.format(date));
+        /*-------------------------------- comparando datas menores e maiores -----------------------*/
 
-        System.out.println("Objeto date: "+simpleDateFormat.parse("1987-05-18 11:34.15"));
+        SimpleDateFormat dataSimples = new SimpleDateFormat("dd/MM/yyyy");
+
+        Date dataVencimentoBoleto = dataSimples.parse("25/05/2023");
+
+        Date dataAtualHoje = dataSimples.parse("23/05/2023");
+
+        if (dataVencimentoBoleto.after(dataAtualHoje)){
+            System.out.println("Boleto não vencido");
+        }else{
+            System.out.println("Boleto vencido");
+        }
+
     }
 }
